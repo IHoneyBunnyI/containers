622fcec16c9c71803c80de46d74c19d035ce6370
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-10-15 11:45:17 +0300

    start rb_tree_node

--- rbtree/RedBlackTree.hpp
+++ rbtree/RedBlackTree.hpp
@@ -1,18 +1,41 @@
-#ifndef REDBLACKTREE_HPP
-#define REDBLACKTREE_HPP
+#ifndef rEDBLACKTrEE_HPP
+#define rEDBLACKTrEE_HPP
 
 #include "enable_if.hpp"
 #include <iostream>
 
 namespace ft
 {
-enum rb_tree_color { RED = false, BLACK = true };
+enum rb_tree_color { red = false, black = true };
 
+template <class T>
+class rb_tree_node
+{
+	public:
+		T val;
+		rb_tree_node<T>* parent;
+		rb_tree_node<T>* left;
+		rb_tree_node<T>* right;
+		rb_tree_color color;
+	
+};
+
+template <class T>
+class rb_tree_iterator
+{
+
+};
+
+template <class T>
+class rb_tree_const_iterator
+{
+
+};
 
 template<typename Key, typename Val, typename KeyOfValue, typename Compare, typename Alloc = std::allocator<Val> >
-class RedBlackTree
+class redBlackTree
 {
-	typedef typename Alloc::template rebind<Rb_tree_node<Val> >::other _Node_allocator;
+	typedef typename Alloc::template rebind<rb_tree_node<Val> >::other _Node_allocator;
 	public:
 		typedef Key key_type;
 		typedef Val value_type;
@@ -20,12 +43,12 @@ class RedBlackTree
 		typedef const value_type* const_pointer;
 		typedef value_type& reference;
 		typedef const value_type& const_reference;
-		typedef Rb_tree_node<Val>* node_type;
+		typedef rb_tree_node<Val>* node_type;
 		typedef size_t size_type;
 		typedef ptrdiff_t difference_type;
 		typedef Alloc allocator_type;
-		typedef Rb_tree_iterator<value_type> iterator;
-		typedef Rb_tree_const_iterator<value_type> const_iterator;
+		typedef rb_tree_iterator<value_type> iterator;
+		typedef rb_tree_const_iterator<value_type> const_iterator;
 		typedef std::reverse_iterator<iterator> reverse_iterator;
 		typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
 		typedef Compare key_compare;
